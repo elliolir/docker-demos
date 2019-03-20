2a70dbbb5fdf2142d95c9594cdc88cf94cf3009a
DImatiBart <dimati.bart@gmail.com>
2019-03-20 15:55:39 +0300

    refactoring

--- 1/index.js
+++ 1/index.js
@@ -1,14 +1,18 @@
-var express = require('express');
-const { writeFile } = require('fs');
+const express = require('express');
+const { writeFile, mkdir } = require('fs');
 const PORT = process.env.PORT || 3000;
 
 let visitCount = 0;
 
-var app = express();
+const app = express();
+
+const logError = (err) => {err && console.log(err)};
+
+mkdir("./logs/", {}, logError);
 
 app.get('/', function(req, res) {
     visitCount++;
-    writeFile("count.txt", visitCount);
+    writeFile("./logs/count.txt", visitCount, logError);
     res.send(`Hello from the Server! This page has been visited ${visitCount} times`);
 });
 
--- 1/test.sh
+++ 1/test.sh
@@ -1,2 +1,2 @@
 docker build -t=example-one .
-docker run -d -p 8080:8080 -e PORT=8080 example-one
+docker run -d -p 8080:4000 -e PORT=4000 example-one
