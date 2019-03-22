df09f283a16484c1b04be17e464d069d4e250cd4
DImatiBart <dimati.bart@gmail.com>
2019-03-22 17:01:26 +0300

    added usage of host folder as a volume, small fixes

--- 2/run.sh
+++ 2/run.sh
@@ -1,3 +1,4 @@
 docker build -t=example-two .
-docker volume create first-volume
-docker run -d -p 8080:4000 -v first-volume:/app/logs -e PORT=4000 example-one
+#docker volume create first-volume
+#docker run -d -p 8080:4000 -v first-volume:/app/data -e PORT=4000 example-two
+docker run -d -p 8080:4000 -v $PWD/../container-data:/app/data -e PORT=4000 example-two
